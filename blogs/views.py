94efe018e5c23a7d6ac73539ab92b8e05e7aa416
torn80beta <103459735+torn80beta@users.noreply.github.com>
2022-08-11 19:51:02 +0300

    BlogPost model and base.html are created

--- blogs/views.py
+++ blogs/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from . models import BlogPost
 
 # Create your views here.
 
 
 def index(request):
     """Application home page"""
-
-    return render(request, 'blogs/index.html')
+    posts = BlogPost.objects.order_by('-date_added')
+    context = {'posts': posts}
+    return render(request, 'blogs/index.html', context)
